46effa9e549bb105b70b9888c12c995886f638ae
engineerjoe440 <engineerjoe440@yahoo.com>
2021-10-17 20:25:17 -0700

    started platformio project

---
+++ platformio/src/main.cpp
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * GBReader - A RGB Pulse-Width-Modulation (PWM) Reader and Digitizer.
+ * 
+ * LICENSE: MIT
+ * AUTHOR: Joe Stanley - Stanley Solutions
+ * PURPOSE: Used to interpret PWM signals with the intent of transmitting MQTT
+ *          commands to other devices on a network to indicate the present state
+ *          of an un-networked RGB controller or LED strip.
+*******************************************************************************/
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
