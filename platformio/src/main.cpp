19ad9810c3a144bcc5bda20aecef4473a2c054c2
engineerjoe440 <engineerjoe440@yahoo.com>
2021-10-17 21:05:17 -0700

    aligned comment

--- platformio/src/main.cpp
+++ platformio/src/main.cpp
@@ -2,7 +2,7 @@
  * GBReader - A RGB Pulse-Width-Modulation (PWM) Reader and Digitizer.
  * 
  * LICENSE: MIT
- * AUTHOR: Joe Stanley - Stanley Solutions
+ * AUTHOR:  Joe Stanley - Stanley Solutions
  * PURPOSE: Used to interpret PWM signals with the intent of transmitting MQTT
  *          commands to other devices on a network to indicate the present state
  *          of an un-networked RGB controller or LED strip.
